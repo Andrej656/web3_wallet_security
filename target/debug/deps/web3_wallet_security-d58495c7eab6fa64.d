12d374a7b654b9a3a5fe68663f4313f402f8466f
Btcdev <chaindev@icloud.com>
2025-02-14 09:15:16 +0100

    Initial commit

---
+++ target/debug/deps/web3_wallet_security-d58495c7eab6fa64.d
@@ -0,0 +1,5 @@
+/home/btcdev/web3_wallet_security/target/debug/deps/libweb3_wallet_security-d58495c7eab6fa64.rmeta: src/main.rs
+
+/home/btcdev/web3_wallet_security/target/debug/deps/web3_wallet_security-d58495c7eab6fa64.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/web3_wallet_security-e9086d93e826dffa.d
@@ -0,0 +1,5 @@
+/home/btcdev/web3_wallet_security/target/debug/deps/libweb3_wallet_security-e9086d93e826dffa.rmeta: src/main.rs
+
+/home/btcdev/web3_wallet_security/target/debug/deps/web3_wallet_security-e9086d93e826dffa.d: src/main.rs
+
+src/main.rs:
